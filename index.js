915227325156b2ae0418da78b5202055e6eee98e
Zane Stock <zanestock3@gmail.com>
2022-05-11 18:50:49 -0500

    commit tree 4ac073909236fd4321102889e5ecaccc13f29d23

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+    this.radius = radius;
+    let pi = Math.PI;
+}
+get area() {
+    return Math.PI * (this.radius * this.radius);
+  }
+  set diameter(newdiameter) {
+    this.radius = newdiameter / 2;
+  }
+  set circumference(newcircumference) {
+    this.radius / Math.PI / 2;
+  }
+  set area(newarea) {
+    this.radius = newarea / Math.PI / Math.sqrt(this.radius);
+  }
+}
+
+circle = new Circle(5);
+Circle.radius();
+Circle.circumference();
+Circle.diameter();
